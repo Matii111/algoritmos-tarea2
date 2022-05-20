9734502ac20dec27b8ac4f653ab09f7eb352e5f8
Mati111 <m.chaparrodaz@uandresbello.edu>
2022-05-19 22:35:42 -0400

    Correciones minimas

--- tarea2_algoritmos.py
+++ tarea2_algoritmos.py
@@ -1,9 +1,17 @@
 #recibe una 'cantidad de tubos' y crea una lista de listas llamada "listaTubos"
 
-cantidadTubos = int(input("Ingresar la cantidad de tubos: "))
+listaConsultas=[]
 listaTubos=[]
-contador=cantidadTubos
 contador_i=0
+contador_ii=0
+contador_iii=0
+contador_iv=0
+contadorComida=0
+
+cantidadTubos = int(input("Ingresar la cantidad de tubos: "))
+
+contador=cantidadTubos
+
 while(contador>0):
     listaTubos.append([contador_i])
     contador-=1
@@ -13,8 +21,9 @@ while(contador>0):
 #luego comprueba mediante un ciclo while que se cumplan las condiciones: 
 # ' 0 <= lanzamiento desde <= lanzamiento hasta <= cantidad de tubos '
 
-contador_ii=0
+
 cantidadLanzamientos = int(input("Cantidad de lanzamientos: "))
+
 while (contador_ii<cantidadLanzamientos):
     print("Ingresar los rangos de lanzamiento {}: ".format(contador_ii+1))
     lanzamientoDesde = int(input("Desde:  "))
@@ -23,7 +32,7 @@ while (contador_ii<cantidadLanzamientos):
         contador_ii+=1
     else: 
         print("Rango de lanzamiento invalido, volver a intentar")
-        
+#el siguiente fragmento se encarga de ingresar un '*' entre los rangos de 'lanzamientoDesde' y 'lanzamientoHasta'        
     for i in listaTubos:
         for j in i :
             if j != "*":    
@@ -33,9 +42,6 @@ while (contador_ii<cantidadLanzamientos):
 #de la misma manera recibe una 'cantidad de consultas' y sus respectivos parametros, luego 
 #comprueba las condiciones : ' 0 <= consulta desde <= consulta hasta <= cantidad de consultas'
 
-contador_iii=0
-contadorComida=0
-listaConsultas=[]
 cantidadConsultas = int(input("Cantidad de consultas: "))
 while(contador_iii<cantidadConsultas):
     print("Ingresar los rangos de la consulta {}: ".format(contador_iii+1))
@@ -45,16 +51,17 @@ while(contador_iii<cantidadConsultas):
         contador_iii+=1
     else:
         print("Rango de consulta invalido, volver a intentar")
-
+#en esta seccion se buscan todos los '*' contandolos y separandolos en argumentos dentro de una lista segun la consulta
     for i in listaTubos:        
             for j in i :
                 if (j=="*" and i[0]>=consultaDesde and i[0]<=consultaHasta):  
                     contadorComida+=1
     listaConsultas.append(contadorComida)
     contadorComida = 0
-
-                        
-print(listaConsultas)            
+#print de los resultados de las consultas 
+for i in listaConsultas:
+    contador_iv+=1
+    print("Resultado consulta {}: {}".format(contador_iv,i))
 
 # print("\n\
 #          | | | | | | | |\n\
