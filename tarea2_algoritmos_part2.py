2bd7f82ac99372a0c5cb2e5e3f5897e98754df5d
Matias Chaparro <101642846+Matii111@users.noreply.github.com>
2022-05-30 21:15:07 -0400

    Codigo para entrega final

--- tarea2_algoritmos_part2.py
+++ tarea2_algoritmos_part2.py
@@ -1,18 +1,15 @@
-n = 3 #int(input("Ingrese un 'n' para el tamaño del bloque: "))
-l = [['0','0','0']]*n
-h=0
-v=0
-
-for i in l :
-    if h<1:
-        i[h] = "*"
-    h+=1
-
-
-
-for i in l:
-    for j in i:        
-        print(j,end='')
-    print("\n")
-        
-
+def espacioDominoPD ( n ) :
+    if n <= 2:
+        return n 
+    else :
+        lista = [0]*( n + 1)
+        lista [1] = 1
+        lista [2] = 2
+        lista [3] = 3
+    i = 4
+    while i <= n :
+        lista [ i ] =  lista [i-1] + lista [i - 3]
+        i = i + 1
+    return lista [-2]
+i = int(input("ingrese la cantidad de columnas del bloque: "))
+print("La cantidad de maneras en la cual se puede rellenar el bloque de 3X1 es de:\n",espacioDominoPD(i))
